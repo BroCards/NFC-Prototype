98156d231ce9488a18b5558fadea1bd6eeeb00b1
warmdaylight <Chitphentom@Settasits-MacBook-Pro.local>
2016-10-30 16:29:14 +0900

    Wifi inspection implementation

--- src/main/java/com/example/chitphentom/nfcprototype/MainActivity.java
+++ src/main/java/com/example/chitphentom/nfcprototype/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.chitphentom.nfcprototype;
 
+import android.content.Context;
 import android.content.pm.ActivityInfo;
+import android.net.wifi.WifiManager;
+import android.net.wifi.WifiInfo;
+import android.nfc.NfcAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,14 +13,20 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-// RNG Text
-import com.example.chitphentom.nfcprototype.RNGText;
+// toasts
+import android.widget.Toast;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
     Button Refresh;
     TextView RNGField, IPAddr, SSID, P_IP, P_SSID, P_RNGText;
     String rngStr;
+
+    // network information
+    int IP;
+    String NETNAME;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +52,10 @@ public class MainActivity extends AppCompatActivity {
         P_SSID = (TextView) findViewById(R.id.ReceivedDomain);
         P_RNGText = (TextView) findViewById(R.id.ReceivedRNG);
 
+        // class globals
+        IP = 0;
+        NETNAME = "";
+
         refresh();
     }
 
@@ -50,10 +64,36 @@ public class MainActivity extends AppCompatActivity {
         rngStr = RNGText.rng_alphanum(5);
         RNGField.setText(rngStr);
 
-        // make sure NFC is on
-        
+        // Get Network Information
+        WifiManager wifiObj = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+        WifiInfo wifiInf = wifiObj.getConnectionInfo();
 
-        // TODO: Get Network Infomation
+        IP = wifiInf.getIpAddress();
+
+        if (IP != 0) {
+            IPAddr.setText(String.format(Locale.ENGLISH, "%d.%d.%d.%d", (IP & 0xff),(IP >> 8 & 0xff),(IP >> 16 & 0xff),(IP >> 24 & 0xff)));
+            NETNAME = wifiInf.getSSID();
+            SSID.setText(NETNAME);
+        } else {
+            NETNAME = "";
+            IPAddr.setText("[Disconnected]");
+            SSID.setText("");
+        }
+
+        // TODO: nfc
+        NfcAdapter adapter = NfcAdapter.getDefaultAdapter(this);
+
+        if (adapter != null && adapter.isEnabled()) {
+            Toast.makeText(this, "NFC is online!", Toast.LENGTH_LONG).show();
+        } else {
+            // something wrong
+            Toast.makeText(this, "NFC is not available or disabled", Toast.LENGTH_LONG).show();
+            P_RNGText.setText("NFC N/A");
+        }
+
+    }
+
+    private void NFCLoop() {
 
     }
 }
